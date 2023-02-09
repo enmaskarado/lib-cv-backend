26cb39ea5a945feceb767e084000225c20213b27
melany estevez camata <elibialany@gmail.com>
2023-02-08 22:26:18 -0400

    add Dockerfile

--- backend/model/certificate.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const certificateSchema = new Schema({
-name: String,
-image: String
-})
-
-module.exports = mongoose.model('Certificate', certificateSchema)
